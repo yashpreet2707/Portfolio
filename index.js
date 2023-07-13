2ce4a7127b9740dd82bcc5235acdcaada4dae4b6
yashpreet2707 <yashpreet1686@gmail.com>
2023-07-14 03:32:49 +0530

    decluttered code

---
+++ index.js
@@ -0,0 +1,26 @@
+// preloader 
+
+const preloader = document.querySelector(".preloader") ;
+    window.addEventListener("load", ()=>{
+        preloader.style.display = "none" ;
+    })
+
+// title change 
+
+    document.addEventListener('visibilitychange',
+    function () {
+        if ( document.visibilityState === "visible" ) {
+            document.title = "Portfolio | Yashpreet Singh" ;
+        } 
+        else {
+                document.title = "Come back to Portfolio";
+            }
+    }) ;
+
+
+// vanilla tilt js 
+
+    VanillaTilt.init(document.querySelector(".yash-img"), {
+	    max: 25,
+		speed: 400
+	});
