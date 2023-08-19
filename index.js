68f0ce03d2a7b79453167713f5447985bd305927
yashpreet2707 <yashpreet1686@gmail.com>
2023-08-19 14:12:13 +0530

    removed preloader

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 // preloader 
 
-const preloader = document.querySelector(".preloader") ;
-    window.addEventListener("load", ()=>{
-        preloader.style.display = "none" ;
-    })
+// const preloader = document.querySelector(".preloader") ;
+//     window.addEventListener("load", ()=>{
+//         preloader.style.display = "none" ;
+//     })
 
 // title change 
 
-    document.addEventListener('visibilitychange',
-    function () {
-        if ( document.visibilityState === "visible" ) {
-            document.title = "Portfolio | Yashpreet Singh" ;
-        } 
-        else {
-                document.title = "Come back to Portfolio";
-            }
-    }) ;
+document.addEventListener('visibilitychange',
+function () {
+    if ( document.visibilityState === "visible" ) {
+        document.title = "Portfolio | Yashpreet Singh" ;
+    } 
+    else {
+            document.title = "Come back to Portfolio";
+        }
+}) ;
 
 
 $('#menu').click(function () {
-    $(this).toggleClass('fa-times') ;
-    $(".nav-part").toggleClass('hello') ;
-    $(".header").toggleClass('header-click') ;
-    $(".navbar").toggleClass('navbar-click') ;
-    $(".nav-ul").toggleClass('nav-ul-click') ;
-    $("#navbar-h2").toggle() ;
+$(this).toggleClass('fa-times') ;
+$(".nav-part").toggleClass('hello') ;
+$(".header").toggleClass('header-click') ;
+$(".navbar").toggleClass('navbar-click') ;
+$(".nav-ul").toggleClass('nav-ul-click') ;
+$("#navbar-h2").toggle() ;
 }) ;
 
 
@@ -32,9 +32,9 @@ $('#menu').click(function () {
 
 // vanilla tilt js 
 
-    VanillaTilt.init(document.querySelector(".yash-img"), {
-	    max: 25,
-		speed: 400
-	});
+VanillaTilt.init(document.querySelector(".yash-img"), {
+    max: 25,
+    speed: 400
+});
 
 
