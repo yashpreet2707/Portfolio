14b297b98f9ca0fbbe770d0a0178be4fd0106c61
Yashpreet Singh <yashpreet1686@gmail.com>
2025-06-03 00:45:21 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex justify-around text-5xl font-bold my-10'>
+      App
+    </div>
+  )
+}
+
+export default App
